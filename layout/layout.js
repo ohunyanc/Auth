cba00937788220533e102180aa08f07c8ee84714
ohunyanc <wildcardcharly@outlook.com>
2024-03-10 09:06:48 +0100

    commit

---
+++ layout/layout.js
@@ -0,0 +1,27 @@
+import styles from '../styles/Layout.module.css'
+
+
+export default function Layout({ children }) {
+    return (
+        <div className="flex h-screen">
+            <div className="m-auto bg-slate-50 rounded-md w-3/5 h-5/5 grid lg:grid-cols-2 border border-emerald-500">
+                <div className={styles.imgStyle}>
+                    <div className={styles.cartoonImg}>
+                        <div className={styles.cloud_one}>
+
+                        </div>
+                        <div className={styles.cloud_two}>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="right flex flex-col justify-evenly">
+                    <div className="text-center py-10">
+                        {children}
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
